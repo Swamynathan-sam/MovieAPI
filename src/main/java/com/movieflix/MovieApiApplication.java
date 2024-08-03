55cb95d1208769364fc35dcf01b483f7af0b0a5a
swamynathan <swamynathansam7@gmail.com>
2024-08-03 15:16:01 +0530

    added jwt security

--- src/main/java/com/movieflix/MovieApiApplication.java
+++ src/main/java/com/movieflix/MovieApiApplication.java
@@ -2,8 +2,9 @@ package com.movieflix;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.movieflix")
 public class MovieApiApplication {
 
 	public static void main(String[] args) {
